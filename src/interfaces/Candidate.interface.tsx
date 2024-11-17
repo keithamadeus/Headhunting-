4acd142370b3645d0b94327b61c709eaef4b572c
Keith <keith.amadeus.williams@gmail.com>
2024-11-17 15:30:49 -0700

    added canidate interface, along with updates to gitignore

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,10 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export default interface Candidate {
+    readonly name: string;
+    readonly username: string;
+    readonly location: string | null;
+    readonly avatar: string;
+    readonly email: string | null;
+    readonly html_url: string;
+    readonly company: string | null;
+    }
