13e23c07ec65bc96cc09a5afe2eaf8c58d8dfb96
Keith <keith.amadeus.williams@gmail.com>
2024-11-18 12:39:40 -0700

    started CandidtateSerch.tsx, more updates to get working.

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,7 +1,26 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
 
+import Candidate from '../interfaces/Candidate.interface';
+
 const CandidateSearch = () => {
+  const [candidates, setCandidates] = useState<Candidate>({
+    name: '',
+    username: '',
+    location: '',
+    avatar: '',
+    email: '',
+    html_url: '',
+    company: '',
+  });
+
+  // Fetch data from the GitHub API when the component mounts
+  const [candidates, setCandidates] = useState<Candidate[]>([]);
+
+  // Implement a function to fetch a single user from the GitHub API
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+
   return <h1>CandidateSearch</h1>;
 };
 
