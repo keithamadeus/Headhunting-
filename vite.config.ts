0e7f15fe6b892f5d3f38fa58baf0363e1a21bb65
Keith <keith.amadeus.williams@gmail.com>
2024-11-27 02:43:46 -0700

    updated vite config for render

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  envDir: './environment',
-  plugins: [react()],
   server: {
     port: 3000,
-    open: true
-  }
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
